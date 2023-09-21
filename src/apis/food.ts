7105a2fa4fdd53c0dcf1fbab1d98b660eb25a6c5
Nghi Nguyen <nguyennghi3489@gmail.com>
2023-09-21 21:49:47 +0700

    fix: correct the get food api url

--- src/apis/food.ts
+++ src/apis/food.ts
@@ -12,7 +12,7 @@ export const getAllCategories = async (): Promise<ICategory[]> => {
 
 export const getAllFoods = async (): Promise<IFood[]> => {
   const response = await fetch(
-    "https://run.mocky.io/v3/f25ced0a-9ff7-4996-bdc7-430f281c48db"
+    "https://run.mocky.io/v3/a24cfec5-f76c-410b-a5ac-9f63fab28abb"
   );
   const foods = await response.json();
 
