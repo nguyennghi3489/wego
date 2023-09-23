a58d39f03c9213b2bd27b244be503949cb9b7add
Nghi Nguyen <nguyennghi3489@gmail.com>
2023-09-23 20:20:16 +0700

    feat: add type for api response

--- src/apis/food.ts
+++ src/apis/food.ts
@@ -5,7 +5,7 @@ export const getAllCategories = async (): Promise<ICategory[]> => {
   const response = await fetch(
     "https://run.mocky.io/v3/f25ced0a-9ff7-4996-bdc7-430f281c48db"
   );
-  const categories = await response.json();
+  const categories = (await response.json()) as ICategory[];
 
   return categories;
 };
@@ -14,7 +14,7 @@ export const getAllFoods = async (): Promise<IFood[]> => {
   const response = await fetch(
     "https://run.mocky.io/v3/a24cfec5-f76c-410b-a5ac-9f63fab28abb"
   );
-  const foods = await response.json();
+  const foods = (await response.json()) as IFood[];
 
   return foods;
 };
