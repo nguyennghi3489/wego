b639276bde4875bda72d2c45853d5e4ee9398758
Nghi Nguyen <nguyennghi3489@gmail.com>
2023-09-25 14:06:56 +0700

    feat: simulate the food pagination api

--- src/apis/food.ts
+++ src/apis/food.ts
@@ -1,5 +1,10 @@
+import { QueryFunctionContext } from "react-query";
 import { ICategory } from "../models/category";
 import { IFood } from "../models/food";
+import { IPagination } from "../models/common";
+
+let allFoods: IFood[] = [];
+const PAGE_LIMIT = 10;
 
 export const getAllCategories = async (): Promise<ICategory[]> => {
   const response = await fetch(
@@ -15,6 +20,21 @@ export const getAllFoods = async (): Promise<IFood[]> => {
     "https://run.mocky.io/v3/a24cfec5-f76c-410b-a5ac-9f63fab28abb"
   );
   const foods = (await response.json()) as IFood[];
-
   return foods;
+};
+
+export const getFoodAtPage = async ({
+  queryKey,
+}: QueryFunctionContext<[string, number]>): Promise<IPagination<IFood[]>> => {
+  const [_, page] = queryKey;
+
+  if (allFoods.length <= 0) {
+    allFoods = await getAllFoods();
+  }
+  const hasNextPage = page * PAGE_LIMIT < allFoods.length;
+
+  return {
+    items: allFoods.slice(page * PAGE_LIMIT, (page + 1) * PAGE_LIMIT),
+    hasNextPage,
+  };
 };
